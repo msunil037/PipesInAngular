e16f98318c68a5d6b91e4be1b6cc1ec3a9bc1698
Sunil Kumar Majhi <msunil037@gmail.com>
2023-01-23 23:20:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'PipesInAngular';
+  today = new Date();
+  a: number = 0.259;
+  b: number = 1.3495;
+  pi: number = 3.14159265359;
+  value!: string;
+  change(value: string) {
+    this.value = value;
+  }
 }
